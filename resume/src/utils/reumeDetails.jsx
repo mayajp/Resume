e4df577321413ee5ea5a139a82cb97a7f6fb33d6
MayaJp <mayajayaprakash@gmail.com>
2021-06-10 04:44:41 -0500

    added resumeDetails

---
+++ resume/src/utils/reumeDetails.jsx
@@ -0,0 +1,37 @@
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+export default {
+    name:'Maya Jayaprakash',
+    title:'Front-End Developer',
+    birthday:'16 December 1981',
+    job:'Freelancer',
+    email:'mayajayaprakash@gmail.com',
+    address:'4205 Privacy Hedge',
+    phone:'408 6699 509',
+    socials: {
+      Facebook:{
+       link:'https://www.facebook.com',
+       text:'Myfacebook',
+       icon: <FacebookIcon/>,
+    },
+       Twitter:{
+        link:'https://www.facebook.com',
+        text:'MyTwitter',
+        icon: <TwitterIcon/>,
+     },
+     LinkedIn:{
+        link:'https://www.facebook.com',
+        text:'MyLinkedIn',
+        icon: <LinkedInIcon/>,
+     },
+     Github:{
+        link:'https://www.facebook.com',
+        text:'MyGitHub',
+        icon: <GitHubIcon/>,
+     }
+
+}
+}
